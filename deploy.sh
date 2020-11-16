5dd779d896e582f8f07790cf3b388e23f0f0d27d
Xiaoxi Wang <xwang@synamedia.com>
2020-11-15 22:15:51 -0500

    fixed typo kubectl set image

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@ docker build -t stacyxixi/multi-serve:lastest -t stacyxixi/multi-server:$SHA -f 
 docker build -t stacyxixi/multi-worker:lastest -t stacyxixi/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 # Take those images and push them to docker hub
-docker push stacyxixi/multi-client:lastest
+docker push stacyxixi/multi-client:latest
 docker push stacyxixi/multi-client:$SHA
 docker push stacyxixi/multi-server:latest
 docker push stacyxixi/multi-server:$SHA
 docker push stacyxixi/multi-worker:latest
 docker push stacyxixi/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set images deployments/server-deployment server=stacyxixi/multi-server:$SHA
-kubectl set images deployments/worker-deployment worker=stacyxixi/multi-worker:$SHA
-kubectl set images deployments/client-deployment client=stacyxixi/multi-client:$SHA
+kubectl set image deployments/server-deployment server=stacyxixi/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=stacyxixi/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=stacyxixi/multi-client:$SHA
